6ad2323112713712e693802518983f474bee0ac2
Nursetiana Situmorang <45727455+nursetiana09@users.noreply.github.com>
2018-12-30 19:28:05 +0700

    Add files via upload

---
+++ memunculkan variabel yang di inginan.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+const float phi = 3.14;
+const char Nama [] = "nursetiana";
+const char catatan [] = "mempelajari setiap bidang IT";
+const char Hobby [] = "olahraga";
+float luas;
+int main()
+{
+    system("color B");
+    printf("%f\n",phi);
+    float luas = 7 * 7 * phi;
+    luas = 7 * 7 * phi;
+    printf("%f\n",luas);
+    printf("%s\n",Nama);
+    printf("%s\n",catatan);
+    printf("%s\n",Hobby);
+    getch();
+    return 0;
+}
